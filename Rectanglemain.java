0a01dd901a9809cdb8491d055af873ff9842de46
NileshGandhi <nilesh.gandhi1@wipro.com>
2024-09-04 12:38:22 +0000

    Java file added

---
+++ Rectanglemain.java
@@ -0,0 +1,32 @@
+class Rectangle{
+	int length, width;
+
+	void SetValue(int length, int width){
+	this.length = length;
+	this.width = width;
+	
+}
+
+	void area(){
+		System.out.println("Area of rec " + this.length * this.width);
+	}
+	void perimeter(){
+		System.out.println("Perimeter of rec " + 2*(this.length* this.width));
+	}
+
+
+}
+
+
+
+class Rectanglemain{
+	
+	public static void main(String args[]){
+		Rectangle r = new Rectangle();
+			
+			r.SetValue(5,8);
+			r.area();
+			r.perimeter();
+	}
+}
+
