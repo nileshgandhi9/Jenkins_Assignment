52875705edc8f304c103071ba2df76b0ad735f8d
nileshgandhi9 <132073621+nileshgandhi9@users.noreply.github.com>
2024-09-05 10:14:35 +0000

    Rectanglemain.java

--- Rectanglemain.java
+++ Rectanglemain.java
@@ -4,20 +4,14 @@ class Rectangle{
 	void SetValue(int length, int width){
 	this.length = length;
 	this.width = width;
-	
 }
-
 	void area(){
 		System.out.println("Area of rec " + this.length * this.width);
 	}
 	void perimeter(){
 		System.out.println("Perimeter of rec " + 2*(this.length* this.width));
 	}
-
-
 }
-
-
 
 class Rectanglemain{
 	
